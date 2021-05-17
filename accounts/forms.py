74858d3abb93b4b7dc0115bd0d3c3a5f72f297e2
Adam Zmuda <adzik007@gmail.com>
2021-05-17 17:58:24 +0200

    removed placeholders

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,51 +5,43 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-# START EXAMPLE LOGIN FORM #
 class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder" : "Username",
                 "class": "form-control"
             }
         ))
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password",
                 "class": "form-control"
             }
         ))
-# END EXAMPLE LOGIN FORM #
 
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder" : "Username",
                 "class": "form-control",
             }
         ))
     email = forms.EmailField(
         widget=forms.EmailInput(
             attrs={
-                "placeholder" : "Email",
                 "class": "form-control",
             }
         ))
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password",
                 "class": "form-control",
             }
         ))
     password2 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password check",
                 "class": "form-control",
             }
         ))
