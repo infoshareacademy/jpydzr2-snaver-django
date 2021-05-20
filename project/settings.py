df6304c5814441d465621163b204755b727eef1b
Robert <robert.rachubinski@gmail.com>
2021-05-20 22:22:08 +0200

    Created appropriate redirection after loggin out.

--- project/settings.py
+++ project/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.join(BASE_DIR, 'project')
 TEMPLATES_DIR = os.path.join(PROJECT_DIR, 'templates')
+LOGOUT_REDIRECT_URL = 'login'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
