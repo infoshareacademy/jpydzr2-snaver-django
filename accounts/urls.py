6a22333ab02fb873a07ee101cc68ab8889b1a44c
Joanna <joanna.angua@gmail.com>
2021-05-21 22:17:45 +0200

    account_activation_sent path added

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import login_view, registration_view
 from django.contrib.auth.views import LogoutView
 
+
 urlpatterns = [
     path('login/', login_view, name="login"),
     path('registration/', registration_view, name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path('account_activation_sent/', registration_view.account_activation_sent, name="account_activation_sent"),
 ]
