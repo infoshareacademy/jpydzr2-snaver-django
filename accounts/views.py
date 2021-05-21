930dddda50c6dbcea014b6a05f59e168604813f3
Joanna <joanna.angua@gmail.com>
2021-05-21 22:29:22 +0200

    activate view added, account activation invalid template added

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,12 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from project.tokens import account_activation_token
 from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 def login_view(request):
     form = LoginForm(request.POST or None)
@@ -41,7 +47,7 @@ def registration_view(request):
 
             current_site = get_current_site(request)
             subject = 'Activate Your MySite Account'
-            message = render_to_string('account_activation_email.html', {
+            message = render_to_string('accounts/account_activation_email.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
@@ -64,3 +70,20 @@ def registration_view(request):
     }
 
     return render(request, "accounts/register.html", context=context)
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.email_confirmed = True
+        user.save()
+        login(request, user)
+        return redirect('home')
+    else:
+        return render(request, 'accounts/account_activation_invalid.html')
