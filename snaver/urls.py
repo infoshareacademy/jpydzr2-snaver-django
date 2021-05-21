6e82160532347ccf8cad9b8bab2b29bb4efb18ac
Robert <robert.rachubinski@gmail.com>
2021-05-21 19:45:05 +0200

    In views.py: @login_required. In settings.py: LOGIN_URL = reverse_lazy('login')

--- snaver/urls.py
+++ snaver/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
 
 from snaver import views
 from snaver.views import CategoryListView
@@ -6,5 +8,5 @@ from snaver.views import CategoryListView
 urlpatterns = [
     path('', views.index, name='home'),
     path('', views.index, name='logout'),
-    path('budget', CategoryListView.as_view(), name='budget')
+    path('budget', CategoryListView.as_view(), name='budget'),
 ]
