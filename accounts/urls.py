6b01fc10d54091a81394fd59cd6860c0427d76c2
Joanna <joanna.angua@gmail.com>
2021-05-22 09:46:12 +0200

    url to activate view added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import login_view, registration_view
+from .views import login_view, registration_view, activate
 from django.contrib.auth.views import LogoutView
-
+from django.conf.urls import url
 
 urlpatterns = [
     path('login/', login_view, name="login"),
     path('registration/', registration_view, name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path('account_activation_sent/', registration_view.account_activation_sent, name="account_activation_sent"),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        activate, name='activate'),
 ]
