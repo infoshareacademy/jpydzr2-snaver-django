16551d8d435a987569135336ad9d3dfbae5e174d
Adam Zmuda <adzik007@gmail.com>
2021-05-23 11:27:24 +0200

    fix to login required

--- snaver/urls.py
+++ snaver/urls.py
@@ -4,8 +4,10 @@ from snaver import views
 from snaver.views import CategoryListView
 from snaver.views import ChartsListView
 
+from django.contrib.auth.decorators import login_required
+
 urlpatterns = [
-    path('', CategoryListView.as_view(), name='home'),
+    path('', login_required(CategoryListView.as_view()), name='home'),
     path('', views.index, name='logout'),
     path('budget', CategoryListView.as_view(), name='budget'),
     path('charts', ChartsListView.as_view(), name='charts'),
