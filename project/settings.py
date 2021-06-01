e5347ac2799767f58ee7ae6ebe9ef921901b4a4b
KMdev <karolmularski94@gmail.com>
2021-06-01 13:37:09 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ LOGIN_URL = reverse_lazy('login')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+DISABLE_COLLECTSTATIC=1
 
 try:
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
