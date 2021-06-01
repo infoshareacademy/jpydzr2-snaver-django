095e49916ece2944c8ea3191e501007247a29861
KMdev <karolmularski94@gmail.com>
2021-06-01 13:56:55 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -34,7 +34,7 @@ except KeyError as e:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://snaver.herokuapp.com/']
 
 # Application definition
 
